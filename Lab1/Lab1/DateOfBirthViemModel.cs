39c611a336ec7299c09fe3226c8f2abb65e64267
kirill-kundik <goliathusua@gmail.com>
2018-02-11 00:34:09 +0200

    almost done
    almost done

---
+++ Lab1/Lab1/DateOfBirthViemModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class DateOfBirthViemModel
+    {
+        private DateTime _dateOfBirth;
+        private int _age;
+        private bool _isTodayABday;
+        private string _zodiakWest;
+        private string _zodiakChinese;
+
+        public DateTime DateOfBirth
+        {
+            get { return _dateOfBirth; }
+            set {
+                if(IsDateValid(DateOfBirth))
+                    this._dateOfBirth = DateOfBirth;
+            }
+        }
+
+        public DateOfBirthViemModel(DateTime dateTime) {
+            DateOfBirth = dateTime;
+        }
+
+        private bool IsDateValid (DateTime date)
+        {
+
+            DateTime now = DateTime.Now;    
+
+
+            return false;
+        }
+    }
+}

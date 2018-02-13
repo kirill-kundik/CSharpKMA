0d5efa14566caf068d2c5abacf7e1320a79d888b
Volodymyr Yablonskyi <yablonskyi.v.reg@gmail.com>
2018-02-13 20:18:06 +0200

    Reviewed

--- Lab1/DateOfBirthViemModel.cs
+++ Lab1/DateOfBirthViemModel.cs
@@ -1,108 +1,132 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using Lab1.Annotations;
+
+//Unused usings
 
 namespace Lab1
 {
-    class DateOfBirthViemModel
+    internal class DateOfBirthViemModel:INotifyPropertyChanged
     {
-        private DateTime? _dateOfBirth;
+        private static readonly string[] ChineseZodiaks = { "Rat", "Ox", "Tiger", "Rabbit", "Dragon", "Snake", "Horse", "Goat", "Monkey", "Rooster", "Dog", "Pig" };
+        private static readonly string[] Zodiaks = { "Aquarius", "Pisces", "Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Saggitarius", "Capricorn" };
+
+        private DateTime _dateOfBirth = DateTime.Today;
         private int _age;
-        private bool _isTodayABday = false;
+        private bool _isTodayABday;
         private string _zodiakWest;
         private string _zodiakChinese;
 
-        public int Age
+        public string Age
         {
-            get { return _age; }
-        }
-
-        public bool IsaBDay
-        {
-            get { return _isTodayABday; }
-        }
-
-        public string ZodiacWest { get { return _zodiakWest; } }
-        
-        public string ZodiacChinese { get { return _zodiakChinese; } }
-
-        public DateOfBirthViemModel(DateTime? dateTime) {
-            this._dateOfBirth = dateTime;
-
-            int days = (DateTime.Today - _dateOfBirth).Value.Days;
-            _age = days / 365;
-
-            if (days < 0 || _age > 135)
-                throw new ArgumentException("Illegal Argument Exception");
-
-
-
-            _isTodayABday = _isABDay(_dateOfBirth);
-
-            _zodiakChinese = _chineseZodiak(_dateOfBirth);
-            _zodiakWest = _zodiak(_dateOfBirth);
-
-        }
-
-        private bool _isABDay(DateTime? date)
-        {
-            return DateTime.Today.DayOfYear == date.Value.DayOfYear;
-        }
-
-        private string _chineseZodiak (DateTime? date)
-        {
-            return _chineseZodiaks[(date.Value.Year - 5) % 12];
-        }
-
-        private string _zodiak(DateTime? date)
-        {
-            int day = date.Value.Day;
-            int month = date.Value.Month;
-            int i = 11;
-
-            if (month == 1 || month == 4)
-                if (day >= 20)
-                    return _zodiaks[month - 1];
-                else if (month == 1)
-                    return _zodiaks[_zodiaks.Length - 1];
-                else
-                    return _zodiaks[month - 2];
-
-            else if (month == 2)
-                if (day >= 19)
-                    return _zodiaks[month - 1];
-                else
-                    return _zodiaks[month - 2];
-
-            else if (month == 3 || month == 5 || month == 6)
-                if (day >= 21)
-                    return _zodiaks[month - 1];
-                else
-                    return _zodiaks[month - 2];
-
-            else if (month == 7 || month == 8 || month == 9 || month == 10)
-                if (day >= 23)
-                    return _zodiaks[month - 1];
-                else
-                    return _zodiaks[month - 2];
-
-            else
+            get
             {
-                if (day >= 22)
+                return $"Your age is {_age}";
+            }
+        }
+        public string ZodiacWest
+        {
+            get
+            {
+                return $"Western zodiak: {_zodiakWest}";
+            }
+            private set
+            {
+                _zodiakWest = value;
+                OnPropertyChanged();
+            }
+        }
+        public string ZodiacChinese
+        {
+            get
+            {
+                return $"China zodiak: {_zodiakChinese}";
+            }
+            private set
+            {
+                _zodiakChinese = value;
+                OnPropertyChanged();
+            }
+        }
+        public DateTime DateOfBirth
+        {
+            get { return _dateOfBirth; }
+            set
+            {
+                _dateOfBirth = value;
+                try
                 {
-                    return _zodiaks[month - 1];
+                    UpdateValues();
+                    OnPropertyChanged();
+                    if (_isTodayABday)
+                        MessageBox.Show("Z DNEM NARODZHENNYA ! SLAVA UKRAINE");
                 }
-                else
+                catch (Exception)
                 {
-                    return _zodiaks[month - 2];
+                    MessageBox.Show("You enter illegal date!");
                 }
             }
         }
 
-        private string[] _chineseZodiaks = {"Rat", "Ox", "Tiger", "Rabbit", "Dragon", "Snake", "Horse", "Goat", "Monkey", "Rooster", "Dog", "Pig" };
-        private string[] _zodiaks = {"Aquarius", "Pisces", "Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Saggitarius", "Capricorn" };
+        internal DateOfBirthViemModel()
+        {
+        }
 
+        private void UpdateValues()
+        {
+            int days = (DateTime.Today - _dateOfBirth).Days;
+            var age = days / 365;
+            
+            if (days < 0 || age > 135)
+                throw new ArgumentException("Illegal Argument Exception");
+
+            _age = age;
+            OnPropertyChanged(nameof(Age));
+            _isTodayABday = IsAbDay();
+            ZodiacChinese = ChineseZodiak();
+            ZodiacWest = Zodiak();
+        }
+        //You don't need to pass argument to this method
+        //WrongNaming
+        private bool IsAbDay()
+        {
+            return DateTime.Today.DayOfYear == _dateOfBirth.DayOfYear;
+        }
+        private string ChineseZodiak()
+        {
+            return ChineseZodiaks[(_dateOfBirth.Year - 5) % 12];
+        }
+        private string Zodiak()
+        {
+            int day = _dateOfBirth.Day;
+            int month = _dateOfBirth.Month;
+            //Not used variable
+
+            if (month == 1 || month == 4)
+                return day >= 20 ? Zodiaks[month - 1] : (month == 1 ? Zodiaks[Zodiaks.Length - 1] : Zodiaks[month - 2]);
+            if (month == 2)
+                return day >= 19 ? Zodiaks[month - 1] : Zodiaks[month - 2];
+
+            if (month == 3 || month == 5 || month == 6)
+                return day >= 21 ? Zodiaks[month - 1] : Zodiaks[month - 2];
+
+            if (month == 7 || month == 8 || month == 9 || month == 10)
+                return day >= 23 ? Zodiaks[month - 1] : Zodiaks[month - 2];
+
+            return day >= 22 ? Zodiaks[month - 1] : Zodiaks[month - 2];
+        }
+
+        //This fields should be static
+        
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
--- Lab1/MainWindow.xaml.cs
+++ Lab1/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+//Not used Usings
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Lab1
 {
@@ -23,27 +12,10 @@ namespace Lab1
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new DateOfBirthViemModel();
         }
 
-        private void _DPSelectedDateChanged (object sender, SelectionChangedEventArgs e)
-        {
-
-            DateTime? date = DatePick.SelectedDate;
-
-            try
-            {
-                DateOfBirthViemModel model = new DateOfBirthViemModel(date);
-                TBAge.Text = "Your age is " + model.Age;
-                TBWestZodiak.Text = "Western zodiak: " + model.ZodiacWest;
-                TBChineseZodiak.Text = "China zodiak: " + model.ZodiacChinese;
-                if (model.IsaBDay)
-                    MessageBox.Show("Z DNEM NARODZHENNYA ! SLAVA UKRAINE");
-            }
-            catch (ArgumentException)
-            {
-                MessageBox.Show("You enter illegal date!");
-            }
-
-        }
+        //Not MVVM. Moved to ViewModel
+        
     }
 }
